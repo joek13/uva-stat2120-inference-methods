ef3e59a5b7c8beeef7a47b11816f931063686e76
Joe Kerrigan <joek1301@gmail.com>
2020-05-06 10:42:22 -0400

    Fix CSS for gh pages

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm ./docs/*.html
 for i in ./src/*.md
 do
 	name="$(basename $i .md)"
-	pandoc -i "$i" -f markdown -t html -s -o "./docs/$name.html" --mathjax --css "../static/style.css"
+	pandoc -i "$i" -f markdown -t html -s -o "./docs/$name.html" --mathjax --css "./static/style.css"
 done
